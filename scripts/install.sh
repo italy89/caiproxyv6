f5147b3271c27ff8f0b8a5cd9731a9169e9c4a02
italy89 <39641172+italy89@users.noreply.github.com>
2022-10-15 20:16:15 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -100,7 +100,7 @@ WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+IP6=2605:a140:2102:6458
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
