af44d04ea5c39586ed3a2d699477ddae9f340014
italy89 <39641172+italy89@users.noreply.github.com>
2022-10-15 19:44:42 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,11 +28,13 @@ install_3proxy() {
 #    systemctl enable 3proxy
     echo "* hard nofile 999999" >>  /etc/security/limits.conf
     echo "* soft nofile 999999" >>  /etc/security/limits.conf
-    echo "net.ipv6.conf.ens3.proxy_ndp=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
     echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
     echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
     echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.disable_ipv6= 0" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.default.disable_ipv6= 0" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.eth0.disable_ipv6= 0" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.lo.disable_ipv6= 0" >> /etc/sysctl.conf
     sysctl -p
     systemctl stop firewalld
     systemctl disable firewalld
@@ -43,7 +45,7 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 2500
+maxconn 1000
 nserver 8.8.8.8
 nserver 8.8.4.4
 nserver 2001:4860:4860::8888
@@ -53,11 +55,11 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth strong
+auth none
 
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
-$(awk -F "/" '{print "auth strong\n" \
+$(awk -F "/" '{print "auth none\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
@@ -70,16 +72,6 @@ $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
-upload_proxy() {
-    local PASS=$(random)
-    zip --password $PASS /home/proxy-installer/proxy.zip /home/proxy-installer/proxy.txt
-    #URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
-
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
-    echo "Password: ${PASS}"
-
-}
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
@@ -130,7 +122,7 @@ systemctl start NetworkManager.service
 ifup ens3
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 65536
+ulimit -n 10048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
 EOF
 
@@ -138,4 +130,3 @@ bash /etc/rc.local
 
 gen_proxy_file_for_user
 
-upload_proxy
